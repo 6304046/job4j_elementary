686c698dabbe22ee9844f0fac3e55a64475fc002
romailin <6304046@mail.ru>
2021-03-02 14:03:43 +0300

    3.3. Разбор первой программы. [#481387]

---
+++ src/ru/job4j/Multiple.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+
+import static java.lang.System.out;
+
+public class Multiple {
+    public static void main(String [] args){
+       System.out.println("1*2=2");
+       System.out.println("1*3=3");
+       System.out.println("1*9=9");
+    }
+}
