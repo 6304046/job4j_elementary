ee62ca4b3c558fbb537d52c635521a9f692fc7b2
romailin <6304046@mail.ru>
2021-03-08 10:52:57 +0300

    6. Идеальный вес [#481367]

--- src/ru/job4j/Fit.java
+++ src/ru/job4j/Fit.java
@@ -10,10 +10,10 @@ public class Fit {
     }
 
     public static void main(String [] args) {
-        short height = 205;
+        short height = 185;
         double man = Fit.manWeight(height);
         System.out.println("Man " + height + " is " + man);
-        short height1 = 180;
+        short height1 = 175;
         double woman = Fit.womanWeight(height);
         System.out.println("Woman " + height1 + " is " + woman);
     }
